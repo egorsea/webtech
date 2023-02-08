f20b0ce10d4fad82da9d8193202c3f9e38e98352
Egor Sadyrev <egor.sadyrev@gmail.com>
2023-02-08 08:54:13 +0400

    'task19_try2'

--- hello.py
+++ hello.py
@@ -2,13 +2,10 @@ def app(environ, start_response):
     """Simplest possible application object"""
 
     status = '200 OK'
-    print(environ)
-    print(environ.PATH_INFO)
-    print(environ.get('QUERY_STRING'))
-    data = "\n".join(environ.get('QUERY_STRING').split("&"))
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
+    # print(environ)
+    # print(environ.PATH_INFO)
+    # print(environ.get('QUERY_STRING'))
+    data = "\n".join(environ.get('QUERY_STRING').split("&")).encode('utf-8')
+    response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return iter([data])
--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ git config --global user.email "your_email@whatever.com"
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn --bind='0.0.0.0:8080' hello:hello.py
+gunicorn --bind='0.0.0.0:8080' hello:app
