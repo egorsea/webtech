2346acdc373e2a95e6ba94086678eee340085c42
Egor Sadyrev <egor.sadyrev@gmail.com>
2023-02-08 12:37:08 +0400

    'task_1_9_11_try_3'

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
 def app(environ, start_response):
-    """Simplest possible application object"""
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
 
-    status = '200 OK'
+# def app(environ, start_response):
+    # """Simplest possible application object"""
+#
+    # status = '200 OK'
     # print(environ)
     # print(environ.PATH_INFO)
     # print(environ.get('QUERY_STRING'))
-    data = "\n".join(environ.get('QUERY_STRING').split("&")).encode('utf-8')
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return iter([data])
+    # data = "\n".join(environ.get('QUERY_STRING').split("&")).encode('utf-8')
+    # response_headers = [('Content-type', 'text/plain')]
+    # start_response(status, response_headers)
+    # return iter([data])
+#
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-git config --global user.name "Your Name"
-git config --global user.email "your_email@whatever.com"
+git config --global user.name "Egor Sadyrev"
+git config --global user.email "egor.sadyrev@gmail.com"
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn --bind='0.0.0.0:8080' hello:app
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & curl 'http://127.0.0.1:8080/?a=1&a=2&b=3'
